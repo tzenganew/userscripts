7eec48e82d364edb750cbdab9a5f592c2ee2c7fb
tzenganew <tzenganew@gmail.com>
2026-01-27 01:22:04 +0800

    Bump version from 0.1.2 to 0.1.3

--- selectquality.user.js
+++ selectquality.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         selectquality
 // @namespace    tzenganew
-// @version      0.1.2
+// @version      0.1.3
 // @description  select quality
 // @author       tzenganew
 // @match        https://live.douyin.com/*
